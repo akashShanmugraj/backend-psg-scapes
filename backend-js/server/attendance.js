475500298d9b955aca71dbd2181a35b19f5e82ea
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-04-05 21:34:07 +0530

    Updated Server code

--- backend-js/server/attendance.js
+++ backend-js/server/attendance.js
@@ -1,37 +1,56 @@
 import mongodb from "mongodb"
 const ObjectID = mongodb.ObjectID
 
-let database
-
+let timetableConnection
+let databaseConnection
 export default class Attendance {
     static async injectDB(conn) {
-        if (database) {
+        if (databaseConnection) {
             return
         }
 
         try {
-            database = await conn.db("c22z2").collection("timetable")
+            databaseConnection = conn
+            timetableConnection = await databaseConnection.db("c22z2").collection("timetable")
         } catch (e) {
             console.error(`Unable to establish connection, ref: ${e}`)
         }
     }
 
-    static async getAllAttendance() {
+    static async getAllTimetable(clcode) {
         try {
-            const timetableData = await database.find({})
+            const timetableData = await databaseConnection.db(`${clcode}`).collection("timetable").find({})
             return timetableData.toArray()
         } catch (e) {
-            console.error("Unable to get attendance")
+            console.error("Unable to get Timetable")
             return {error: e}
         }
     }
-    static async getDayAttendance(day) {
+    static async getDayTimetable(clcode, day) {
         try {
-            const timetableDataOneDay = await database.find({day: day})
-            return timetableDataOneDay
+            console.log(day)
+            const timetableDay = await databaseConnection.db(`${clcode}`).collection("timetable").find({"day": `${day}`})
+            return timetableDay.toArray()
         } catch (e) {
             console.error(e)
 
+        }
+    }
+    static async getStudentAttendance(clcode, rollNumber, courseCode) {
+        try {
+            const studentAttendance = await databaseConnection.db(`${clcode}`).collection(`${courseCode}`).find({"rollNumber": `${rollNumber}`})
+            return studentAttendance.toArray()
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    static async getAllAttendance(clcode, crcode) {
+        try {
+            const studentAttendance = await databaseConnection.db(`${clcode}`).collection(`${crcode}`).find({})
+            return studentAttendance.toArray()
+        } catch (e) {
+            console.error(e)
         }
     }
 }
