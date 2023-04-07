c1665e69e7c2174c9220533629fae3ecce0b3f92
Anandkumar NS <69028746+aknsubbu@users.noreply.github.com>
2023-04-07 17:39:25 +0530

    Add files via upload
    REST API mockup for the PSG Scapes Project

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express=require('express');
-// import express from "express"
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
-// import bodyParser from "body-parser";
-// import mongoose from "mongoose";
-// import api from "./routes/api"
+
 //set up express app
 const app=express();
 
@@ -12,10 +9,10 @@ const app=express();
 app.use(bodyParser.json());
 
 // setting routes folder
-app.use('/api',api);
+app.use('/api',require('./routes/api'));
 
 //connect to mongodb
-mongoose.connect('mongodb+srv://aknsubbu:aknswithA+@learn-mongodb.pghq5gn.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://aknsubbu:aknswithA+@learn-mongodb.pghq5gn.mongodb.net/psg-scapes?retryWrites=true&w=majority');
 
 //listening for requests 
 app.listen(process.env.port || 4000,function(){
@@ -25,6 +22,4 @@ app.listen(process.env.port || 4000,function(){
 //error handling 
 app.use(function(err,req,res,next){
     res.status(422).send({error:err.message});
-    
-    
 });
