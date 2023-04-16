0c24647cd9caf48d5b48784ac03b27c58d7ae0b1
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-04-16 10:41:10 +0530

    Modified the help route and added a testing route for req

--- api/auth.route.js
+++ api/auth.route.js
@@ -1,15 +1,35 @@
-import express from "express"
-import AuthControl from './auth.controller.js'
-import tokenManager from "../auth/tokens.js"
+import express from "express";
+import AuthControl from './auth.controller.js';
+import tokenManager from "../auth/tokens.js";
+
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send("this is psg-scapes api"))
-router.route("/users").get(AuthControl.apiGetAllUsers)
+router.route("/").post((req, res) => {res.send("this is psg-scapes api")})
+router.route("/test").get((req, res) => {
+    const requestObject = {
+        "req.app": req.app,
+        "req.baseurl":req.baseUrl,
+        "req.body": req.body,
+        "req.cookie": req.cookie,
+        "req.hostname":req.hostname,
+        "req.ipv6":req.ipv6,
+        "req.params":req.params,
+        "req.path": req.path,
+        "req.protocol": req.protocol,
+        "req.query":req.query,
+        "req.route":req.route,
+        "req.tlsInsecure":req.tlsInsecure,
+        "req.auth":req.auth
+    }
+
+    res.send(requestObject)
+})
+router.route("/users").get(tokenManager.verifyToken, AuthControl.apiGetAllUsers)
 router.route("/users/byrollnumber").post(AuthControl.apiGetOneUserbyRollnumber)
 router.route("/auth/login").post(tokenManager.loginToken)
 router.route("/auth/renew").post(tokenManager.generateToken)
-router.route("/auth/help").get(tokenManager.authHelp)
+router.route("/auth/help").get((req,res) => res.sendfile('authhelp.html'))
 router.route('/auth/verify/text').post(tokenManager.verifyText)
 
 export default router
